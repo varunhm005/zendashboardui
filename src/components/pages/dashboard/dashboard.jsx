c19f89623d15e19a4aa7857554fc0afcb35bce34
varunharikumar <varun.h@stackontech.com>
2024-05-13 23:16:07 +0530

    Added dashboard updates

--- src/components/pages/dashboard/dashboard.jsx
+++ src/components/pages/dashboard/dashboard.jsx
@@ -6,13 +6,66 @@ import { Data } from "./data";
 // import PieChart from "../components/PieChart";
 import { Bar } from "react-chartjs-2";
 import withAuthCheck from '../../Auth/withAuthCheck';
+const moment = require('moment-timezone');
+
 
 
 function Dashboard() {
 
-    
+  moment.tz.setDefault('Asia/Kolkata');
+
+  const [web, setWeb] = useState(0);
+  const [code, setCode] = useState(0);
+  const [weekInfoWeb, setweekInfoWeb] = useState({});
+  const [weekInfoCode, setweekInfoCode] = useState({});
+
+  function getWeekWebKata(week, type) {
+    let firstDay;
+    let lastDay;
+
+    if (type == "next") {
+      firstDay = moment().add(week, 'week').startOf('week');
+      lastDay = moment().add(week, 'week').endOf('week');
+    } else if (type == "previous") {
+      firstDay = moment().subtract(week, 'week').startOf('week');
+      lastDay = moment().subtract(week, 'week').endOf('week');
+    }
+
+    return { firstDay: firstDay.format('DD/MM/YYYY'), lastDay: lastDay.format('DD/MM/YYYY') };
+  }
+
+
+  function getWeekCodeKata(week, type, initial) {
+    console.log("weeeek",week,type)
+    let firstDay;
+    let lastDay;
+
+    if (type == "next") {
+      firstDay = moment().add(week, 'week').startOf('week');
+      lastDay = moment().add(week, 'week').endOf('week');
+      if(!initial){
+        setCode(week+1)
+      }
+    } else if (type == "previous") {
+      firstDay = moment().subtract(week, 'week').startOf('week');
+      lastDay = moment().subtract(week, 'week').endOf('week');
+      if(!initial){
+        setCode(week-1)
+      }
+    }
+
+    setweekInfoCode({ firstDay: firstDay.format('DD/MM/YYYY'), lastDay: lastDay.format('DD/MM/YYYY') })
+    return { firstDay: firstDay.format('DD/MM/YYYY'), lastDay: lastDay.format('DD/MM/YYYY') };
+  }
+  
+  useEffect(() => { 
+    setweekInfoWeb(getWeekWebKata(0,'next', 'initial'))
+    setweekInfoCode(getWeekCodeKata(0, 'next', 'initial'))
+  }, [])
+  
+
   const [chartData, setChartData] = useState({
-    labels: Data.map((data) => data.day), 
+    labels: Data.map((data) => data.day),
     datasets: [
       {
         label: "Users Gained ",
@@ -44,61 +97,62 @@ function Dashboard() {
     }
   };
 
-    return (
-        <section className='dashboard-container' >
-            <div className='text-center' >
-                <h5>Activities</h5>
-            </div>
-            <div className="row justify-content-around">
-                <div className="col-xl-5 column-style">
-                    <div class="d-flex box-style">
-                        <div class="Dashboard_chart__topic__3JSqN">Codekata</div>
-                        <div className='d-flex score-style'>
-                            <div> <span className='score-style-span'  >Today : 0</span> <span> <span className='score-circle-first' ></span> Max Scores </span> </div>
-                            <div> <span className='score-style-span' >Total : 1310</span> <span> <span className='score-circle-second' ></span> Points Earned</span> </div>
-                        </div>
-                    </div>
-                    <Bar
-        data={chartData}
-        options={options}
-      />
 
-                    <div class="Dashboard_space__between__a7GVw">
-                        <div class="" style={{'cursor': 'pointer'}}>
-                            <img class="p-2" src="/Icons/backwardArrow.svg" alt="add/plus"></img>
-                                </div>
-                                <div class="">04/05/2024 - 10/05/2024</div>
-                                <div class="" style={{'cursor': 'pointer'}}>
-                                    <img class="p-2" src="/Icons/forwardArrow.svg" alt="add/plus"></img>
-                                        </div>
-                                        </div>
-                </div>
-                <div className="col-xl-5 column-style">
-                    <div class="d-flex box-style">
-                        <div class="Dashboard_chart__topic__3JSqN">Codekata</div>
-                        <div className='d-flex score-style'>
-                            <div> <span className='score-style-span'  >Today : 0</span> <span> <span className='score-circle-first' ></span> Max Scores </span> </div>
-                            <div> <span className='score-style-span' >Total : 1310</span> <span> <span className='score-circle-second' ></span> Points Earned</span> </div>
-                        </div>
-                    </div>
-                    <Bar
-        data={chartData}
-        options={options}
-      />
-
-                    <div class="Dashboard_space__between__a7GVw">
-                        <div class="" style={{'cursor': 'pointer'}}>
-                            <img class="p-2" src="/Icons/backwardArrow.svg" alt="add/plus"></img>
-                                </div>
-                                <div class="">04/05/2024 - 10/05/2024</div>
-                                <div class="" style={{'cursor': 'pointer'}}>
-                                    <img class="p-2" src="/Icons/forwardArrow.svg" alt="add/plus"></img>
-                                        </div>
-                                        </div>
-                </div>
+  return (
+    <section className='dashboard-container' >
+      <div className='text-center' >
+        <h5>Activities</h5>
+      </div>
+      <div className="row justify-content-around">
+        <div className="col-xl-5 column-style">
+          <div class="d-flex box-style">
+            <div class="Dashboard_chart__topic__3JSqN">Codekata</div>
+            <div className='d-flex score-style'>
+              <div> <span className='score-style-span'  >Today : 0</span> <span> <span className='score-circle-first' ></span> Max Scores </span> </div>
+              <div> <span className='score-style-span' >Total : 1310</span> <span> <span className='score-circle-second' ></span> Points Earned</span> </div>
             </div>
-        </section>
-    )
+          </div>
+          <Bar
+            data={chartData}
+            options={options}
+          />
+
+          <div class="Dashboard_space__between__a7GVw">
+            <div class="" style={{ 'cursor': 'pointer' }}>
+              <img class="p-2" src="/Icons/backwardArrow.svg" alt="add/plus" value={code} onClick={() => getWeekCodeKata(code, 'previous', null)}></img>
+            </div>
+            <div class="" id="web" >{weekInfoCode.firstDay} - {weekInfoCode.lastDay}</div>
+            <div class="" style={{ 'cursor': 'pointer' }}>
+              <img class="p-2" src="/Icons/forwardArrow.svg" alt="add/plus" onClick={() => getWeekCodeKata(code, 'next', null)} ></img>
+            </div>
+          </div>
+        </div>
+        <div className="col-xl-5 column-style">
+          <div class="d-flex box-style">
+            <div class="Dashboard_chart__topic__3JSqN">Codekata</div>
+            <div className='d-flex score-style'>
+              <div> <span className='score-style-span'  >Today : 0</span> <span> <span className='score-circle-first' ></span> Max Scores </span> </div>
+              <div> <span className='score-style-span' >Total : 1310</span> <span> <span className='score-circle-second' ></span> Points Earned</span> </div>
+            </div>
+          </div>
+          <Bar
+            data={chartData}
+            options={options}
+          />
+
+          <div class="Dashboard_space__between__a7GVw">
+            <div class="" style={{ 'cursor': 'pointer' }}  >
+              <img class="p-2" src="/Icons/backwardArrow.svg" alt="add/plus"></img>
+            </div>
+            <div class="" value={web} >{weekInfoWeb.firstDay} - {weekInfoWeb.lastDay}</div>
+            <div class="" style={{ 'cursor': 'pointer' }} >
+              <img class="p-2" src="/Icons/forwardArrow.svg" alt="add/plus"></img>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default withAuthCheck(Dashboard);
--- src/components/pages/leaveApplication/leaveApplication.jsx
+++ src/components/pages/leaveApplication/leaveApplication.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './leaveApplication.css'
+import { fetchData, postData } from '../../../api/api'
 
 export default function LeaveApplication() {
 
+    const [data, setData] = useState(null);
     const [showModal, setShowModal] = useState(false);
+
+    const daysRef = useRef(null);
+    const leaveRef = useRef(null);
+    const reasonRef = useRef(null)
 
     const openModal = () => {
         setShowModal(true);
@@ -13,7 +19,62 @@ export default function LeaveApplication() {
         setShowModal(false);
     };
 
-    console.log("showModal", showModal)
+    useEffect(() => {
+        // Function to fetch data from the API
+
+
+        // Call the fetch data function when the component mounts
+        getQuery();
+
+
+        // Clean-up function (optional)
+        return () => {
+            // You can do any clean-up here if necessary
+        };
+    }, []);
+
+    const getQuery = async () => {
+        try {
+            const result = await fetchData('/leave');
+            if (result.code === 200) {
+                setData(result.data);
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const days = daysRef.current.value;
+        const leaveOn = leaveRef.current.value;
+        const reason = reasonRef.current.value;
+
+        // Handle form submission logic here, such as sending the data to a server
+
+        let payload = {
+            days, leaveOn, reason
+        }
+
+        const response = await postData('/leave', JSON.stringify(payload), {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });    
+          if (response.code === 200) {
+              alert(response.message)
+              getQuery()
+              setShowModal(false);
+          } else {
+            // Handle login error
+            alert('Failed while creating query');
+          }
+
+        // You can also reset the form fields here if needed
+        daysRef.current.value = '';
+        leaveRef.current.value = '';
+        reasonRef.current.value = '';
+    };
 
     return (
         <section className="leaveapplication-container">
@@ -39,21 +100,21 @@ export default function LeaveApplication() {
                                     </div>
                                 </div>
                                 <div class="modal-body">
-                                    <form class="d-flex justify-content-center flex-column mt-2" autocomplete="off">
+                                    <form class="d-flex justify-content-center flex-column mt-2" autocomplete="off" onSubmit={handleSubmit}>
                                         <div class="leave_select__1wE2t">
                                             <label for="days">Days</label>
-                                            <select name="days" id="days">
+                                            <select name="days" id="days" ref={daysRef} >
                                                 <option value="1">1</option>
                                                 <option value="more">More than one</option>
                                             </select></div><div class="form-group mt-1">
                                             <label for="on" class="label-style mb-0">On</label>
                                             <div>
-                                                <input class="formInputs" name="on" placeholder="On" type="date" value=""></input>
+                                                <input class="formInputs" name="on" ref={leaveRef}  placeholder="On" type="date" ></input>
                                             </div>
                                         </div>
                                         <div class="form-group mt-1">
                                             <label for="reason">Reason</label>
-                                            <textarea class="formInputs" rows="5" name="reason" type="text" placeholder="Enter Reason"></textarea>
+                                            <textarea class="formInputs" rows="5" ref={reasonRef}  name="reason" type="text" placeholder="Enter Reason"></textarea>
                                         </div>
                                         <div class="modal-footer">
                                             <button type="reset" class="btn cancel-btn" onClick={closeModal}>Cancel</button>
@@ -65,6 +126,36 @@ export default function LeaveApplication() {
                         </div>
                     </div>
                 )}
+
+
+                <div class="row leave-component">
+                    <div class="col-xl-12 my-4 parent-column" style={{ 'flex-direction': 'column' }} >
+                        {data ? (
+                            data.map((item, index) => (
+                                <div class="main-content" >
+                                    <div class="left-content">
+                                        <h3>Leave Application</h3>
+                                        <div>
+                                            <span class="batch-details">Reason</span>
+                                            <span class="class-details"> - {item.reason}</span>
+                                        </div>
+                                    </div>
+                                    <div class="right-content">
+                                        <span class="task px-2">Approved</span>
+                                        <div class="leave-details">
+                                            <span class="mx-1 pl-0 pt-0 pb-3" style={{ 'padding-right': '10px' }}>No Of Days: </span>
+                                            <span class="submission mb-2">{item.days}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        ) : (
+                            <div>Loading</div>
+                        )
+                        }
+                    </div>
+                </div>
+
             </div>
         </section>
     )
