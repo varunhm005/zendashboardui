ea8b16dfeca692015b15c9c284021e3a69df88d6
varunharikumar <varun.h@stackontech.com>
2024-05-23 22:02:55 +0530

    Added the dashboard updates

--- src/components/pages/dashboard/dashboard.jsx
+++ src/components/pages/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { fetchData } from '../../../api/api'
 import './dashboard.css'
 import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
@@ -18,53 +19,12 @@ function Dashboard() {
   const [code, setCode] = useState(0);
   const [weekInfoWeb, setweekInfoWeb] = useState({});
   const [weekInfoCode, setweekInfoCode] = useState({});
+  const [webToday, setWebToday] = useState(0);
+  const [webTotal, setWebTotal] = useState(0);
+  const [codeToday, setCodeToday] = useState(0);
+  const [codeTotal, setCodeTotal] = useState(0);
 
-  function getWeekWebKata(week, type) {
-    let firstDay;
-    let lastDay;
-
-    if (type == "next") {
-      firstDay = moment().add(week, 'week').startOf('week');
-      lastDay = moment().add(week, 'week').endOf('week');
-    } else if (type == "previous") {
-      firstDay = moment().subtract(week, 'week').startOf('week');
-      lastDay = moment().subtract(week, 'week').endOf('week');
-    }
-
-    return { firstDay: firstDay.format('DD/MM/YYYY'), lastDay: lastDay.format('DD/MM/YYYY') };
-  }
-
-
-  function getWeekCodeKata(week, type, initial) {
-    console.log("weeeek",week,type)
-    let firstDay;
-    let lastDay;
-
-    if (type == "next") {
-      firstDay = moment().add(week, 'week').startOf('week');
-      lastDay = moment().add(week, 'week').endOf('week');
-      if(!initial){
-        setCode(week+1)
-      }
-    } else if (type == "previous") {
-      firstDay = moment().subtract(week, 'week').startOf('week');
-      lastDay = moment().subtract(week, 'week').endOf('week');
-      if(!initial){
-        setCode(week-1)
-      }
-    }
-
-    setweekInfoCode({ firstDay: firstDay.format('DD/MM/YYYY'), lastDay: lastDay.format('DD/MM/YYYY') })
-    return { firstDay: firstDay.format('DD/MM/YYYY'), lastDay: lastDay.format('DD/MM/YYYY') };
-  }
-  
-  useEffect(() => { 
-    setweekInfoWeb(getWeekWebKata(0,'next', 'initial'))
-    setweekInfoCode(getWeekCodeKata(0, 'next', 'initial'))
-  }, [])
-  
-
-  const [chartData, setChartData] = useState({
+  const [codeChartData, setCodeChartData] = useState({
     labels: Data.map((data) => data.day),
     datasets: [
       {
@@ -79,6 +39,23 @@ function Dashboard() {
       }
     ]
   });
+
+  const [webChartData, setWebChartData] = useState({
+    labels: Data.map((data) => data.day),
+    datasets: [
+      {
+        label: "Points",
+        data: Data.map((data) => data.userGain),
+        backgroundColor: [
+          "rgb(255, 190, 190)"
+        ],
+        borderColor: "rgb(255, 190, 190)",
+        borderWidth: 0,
+
+      }
+    ]
+  });
+
 
   const options = {
     scales: {
@@ -97,6 +74,130 @@ function Dashboard() {
     }
   };
 
+  async function getWeekWebKata(week, type, initial) {
+    let firstDay;
+    let lastDay;
+
+    if (type === "next") {
+      if (!initial) {
+        week = week + 1
+        setWeb(week)
+      }
+      firstDay = moment().add(week, 'week').startOf('week');
+      lastDay = moment().add(week, 'week').endOf('week');
+    } else if (type === "previous") {
+      if (!initial) {
+        week = week - 1
+        setWeb(week)
+      }
+      if (week < 0) {
+        week = Math.abs(week);
+      }
+      firstDay = moment().subtract(week, 'week').startOf('week');
+      lastDay = moment().subtract(week, 'week').endOf('week');
+    }
+
+    setweekInfoWeb({ firstDay: firstDay.format('DD/MM/YYYY'), lastDay: lastDay.format('DD/MM/YYYY') })
+
+    let from = firstDay.format('YYYY-MM-DDTHH:mm:ss.SSS[Z]');
+    let to = lastDay.format('YYYY-MM-DDTHH:mm:ss.SSS[Z]')
+
+    await fetchData(`webkata?from=${from}&to=${to}`)
+      .then(response => {
+        const data = response.data;
+
+        const labels = data.pointDetails[0].data.map((item) => item.day);
+        const userData = data.pointDetails[0].data.map((item) => item.points);
+        setWebToday(data?.pointDetails[0]?.totalPoints ? data.pointDetails[0].totalPoints : 0)
+        setWebTotal(data?.overallPoints ? data.overallPoints : 0)
+
+        setWebChartData({
+          labels: labels,
+          datasets: [
+            {
+              label: "Points",
+              data: userData,
+              backgroundColor: "rgb(255, 190, 190)",
+              borderColor: "rgb(255, 190, 190)",
+              borderWidth: 0,
+            }
+          ]
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching the data', error);
+      });
+
+
+    return { firstDay: firstDay.format('DD/MM/YYYY'), lastDay: lastDay.format('DD/MM/YYYY') };
+  }
+
+
+  async function getWeekCodeKata(week, type, initial) {
+    let firstDay;
+    let lastDay;
+
+    if (type === "next") {
+      if (!initial) {
+        week = week + 1
+        setCode(week)
+      }
+      firstDay = moment().add(week, 'week').startOf('week');
+      lastDay = moment().add(week, 'week').endOf('week');
+    } else if (type === "previous") {
+      if (!initial) {
+        week = week - 1
+        setCode(week)
+      }
+      if (week < 0) {
+        week = Math.abs(week);
+      }
+      firstDay = moment().subtract(week, 'week').startOf('week');
+      lastDay = moment().subtract(week, 'week').endOf('week');
+    }
+
+    setweekInfoCode({ firstDay: firstDay.format('DD/MM/YYYY'), lastDay: lastDay.format('DD/MM/YYYY') })
+
+    let from = firstDay.format('YYYY-MM-DDTHH:mm:ss.SSS[Z]');
+    let to = lastDay.format('YYYY-MM-DDTHH:mm:ss.SSS[Z]')
+
+    await fetchData(`codekata?from=${from}&to=${to}`)
+      .then(response => {
+        const data = response.data;
+
+        const labels = data.pointDetails[0].data.map((item) => item.day);
+        const userData = data.pointDetails[0].data.map((item) => item.points);
+        setCodeToday(data?.pointDetails[0]?.totalPoints ? data.pointDetails[0].totalPoints : 0)
+        setCodeTotal(data?.overallPoints ? data.overallPoints : 0)
+
+        setCodeChartData({
+          labels: labels,
+          datasets: [
+            {
+              label: "Points",
+              data: userData,
+              backgroundColor: "rgb(255, 190, 190)",
+              borderColor: "rgb(255, 190, 190)",
+              borderWidth: 0,
+            }
+          ]
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching the data', error);
+      });
+
+
+    return { firstDay: firstDay.format('DD/MM/YYYY'), lastDay: lastDay.format('DD/MM/YYYY') };
+  }
+
+  useEffect(() => {
+    getWeekWebKata(0, 'next', 'initial')
+    getWeekCodeKata(0, 'next', 'initial')
+  }, [])
+
+
+
 
   return (
     <section className='dashboard-container' >
@@ -108,20 +209,20 @@ function Dashboard() {
           <div class="d-flex box-style">
             <div class="Dashboard_chart__topic__3JSqN">Codekata</div>
             <div className='d-flex score-style'>
-              <div> <span className='score-style-span'  >Today : 0</span> <span> <span className='score-circle-first' ></span> Max Scores </span> </div>
-              <div> <span className='score-style-span' >Total : 1310</span> <span> <span className='score-circle-second' ></span> Points Earned</span> </div>
+              <div> <span className='score-style-span'  >Today : {codeToday}</span> <span> <span className='score-circle-first' ></span> Max Scores </span> </div>
+              <div> <span className='score-style-span' >Total : {codeTotal}</span> <span> <span className='score-circle-second' ></span> Points Earned</span> </div>
             </div>
           </div>
           <Bar
-            data={chartData}
+            data={codeChartData}
             options={options}
           />
 
           <div class="Dashboard_space__between__a7GVw">
             <div class="" style={{ 'cursor': 'pointer' }}>
-              <img class="p-2" src="/Icons/backwardArrow.svg" alt="add/plus" value={code} onClick={() => getWeekCodeKata(code, 'previous', null)}></img>
+              <img class="p-2" src="/Icons/backwardArrow.svg" alt="add/plus" onClick={() => getWeekCodeKata(code, 'previous', null)}></img>
             </div>
-            <div class="" id="web" >{weekInfoCode.firstDay} - {weekInfoCode.lastDay}</div>
+            <div class="" id="web" value={code} >{weekInfoCode.firstDay} - {weekInfoCode.lastDay}</div>
             <div class="" style={{ 'cursor': 'pointer' }}>
               <img class="p-2" src="/Icons/forwardArrow.svg" alt="add/plus" onClick={() => getWeekCodeKata(code, 'next', null)} ></img>
             </div>
@@ -129,24 +230,24 @@ function Dashboard() {
         </div>
         <div className="col-xl-5 column-style">
           <div class="d-flex box-style">
-            <div class="Dashboard_chart__topic__3JSqN">Codekata</div>
+            <div class="Dashboard_chart__topic__3JSqN">Webkata</div>
             <div className='d-flex score-style'>
-              <div> <span className='score-style-span'  >Today : 0</span> <span> <span className='score-circle-first' ></span> Max Scores </span> </div>
-              <div> <span className='score-style-span' >Total : 1310</span> <span> <span className='score-circle-second' ></span> Points Earned</span> </div>
+              <div> <span className='score-style-span'  >Today : {webToday}</span> <span> <span className='score-circle-first' ></span> Max Scores </span> </div>
+              <div> <span className='score-style-span' >Total : {webTotal}</span> <span> <span className='score-circle-second' ></span> Points Earned</span> </div>
             </div>
           </div>
           <Bar
-            data={chartData}
+            data={webChartData}
             options={options}
           />
 
           <div class="Dashboard_space__between__a7GVw">
             <div class="" style={{ 'cursor': 'pointer' }}  >
-              <img class="p-2" src="/Icons/backwardArrow.svg" alt="add/plus"></img>
+              <img class="p-2" src="/Icons/backwardArrow.svg" alt="add/plus" onClick={() => getWeekWebKata(web, 'previous', null)} ></img>
             </div>
             <div class="" value={web} >{weekInfoWeb.firstDay} - {weekInfoWeb.lastDay}</div>
             <div class="" style={{ 'cursor': 'pointer' }} >
-              <img class="p-2" src="/Icons/forwardArrow.svg" alt="add/plus"></img>
+              <img class="p-2" src="/Icons/forwardArrow.svg" alt="add/plus" onClick={() => getWeekWebKata(web, 'next', null)} ></img>
             </div>
           </div>
         </div>
